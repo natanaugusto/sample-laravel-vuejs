3f7925b786ef375d92b8288581c46231962edbc5
Natan Augusto <natanaugusto@gmail.com>
2019-08-13 11:54:25 -0300

    Permissions added
    The spatie/laravel-permission installed
    
    RolesAndPermissions seeder added

---
+++ database/seeds/RolesAndPermissionsSeeder.php
@@ -0,0 +1,52 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Role;
+use Spatie\Permission\Models\Permission;
+
+class RolesAndPermissionsSeeder extends Seeder
+{
+    public static $roles = [
+        'admin' => '*',
+    ];
+
+    public static $permissions = [
+        'list products',
+        'view products',
+        'add product',
+        'update product',
+        'delete product',
+        'list categories',
+        'view categories',
+        'add category',
+        'update category',
+        'delete category',
+    ];
+
+
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        // Reset cached roles and permissions
+        app()[\Spatie\Permission\PermissionRegistrar::class]->forgetCachedPermissions();
+
+        foreach(self::$permissions as $permission) {
+            Permission::firstOrCreate(['name' => $permission]);
+        }
+
+        foreach(self::$roles as $role => $permission) {
+            $role = Role::firstOrCreate(['name' => $role]);
+            if(!is_null($permission)) {
+                $role->givePermissionTo(
+                    $permission === '*' ? 
+                    Permission::all() :
+                    $permission
+                );
+            }
+        }
+    }
+}
